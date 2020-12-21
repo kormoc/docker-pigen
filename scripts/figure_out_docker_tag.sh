5693fa605dd818208dab9efa07f99062f16cdc48
Rob Smith <kormoc@gmail.com>
2020-12-20 17:08:16 -0800

    Move to a single workflow define for both tags and main

--- scripts/figure_out_docker_tag.sh
+++ scripts/figure_out_docker_tag.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-
 case "${GITHUB_REF}" in
+    refs/heads/main)
+        echo "Using tag 'latest'" >&2
+        echo "::set-output name=TAG::latest"
+        ;;
     /refs/tags/*)
+        echo "Using tag '${GITHUB_REF/refs\/tags\//}'" >&2
         echo "::set-output name=TAG::${GITHUB_REF/refs\/tags\//}"
         ;;
-    refs/heads/*)
-        echo "::set-output name=TAG::${GITHUB_REF/refs\/heads\//}"
-        ;;
     *)
-        echo "::set-output name=TAG::${GITHUB_REF}"
+        echo "I don't know what to do with '${GITHUB_REF}'" >&2
+        exit 1
         ;;
 esac
